61d54efa743189cb43005ef221427bd200207359
Tania-2004 <twilkes2@terpmail.umd.edu>
2025-04-08 23:39:00 -0400

    player upadate
    Camara movement still needs to be Debuged but movement with arrows are good!

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class PlayerControl : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public Transform orientation;
+
+    float horizontalInput;
+    float verticalInput;
+
+    Vector3 moveDircetion;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        MyInput();
     }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+    }
+    private void MovePlayer()
+    {
+        //Cam Movement direction
+        moveDircetion = orientation.forward * verticalInput + orientation.right * horizontalInput;
+
+        rb.AddForce(moveDircetion.normalized * moveSpeed * 5f, ForceMode.Force);
+    }
+
 }
